3e9cee32500c1df50689fbc50227f8410325ada7
Ola <cencora.aleksandra@gmail.com>
2024-07-08 12:27:31 +0200

    Add navigation

---
+++ src/components/features/Navigation/Navigation.tsx
@@ -0,0 +1,73 @@
+import styles from "./Navigation.module.scss";
+import { NavLink } from "react-router-dom";
+import { IoClose } from "react-icons/io5";
+
+interface NavigationProps {
+  isNavShown: boolean;
+  setIsNavShown: (state: boolean) => void;
+}
+
+const Navigation = ({ isNavShown, setIsNavShown }: NavigationProps) => {
+  return (
+    <nav
+      className={`${
+        isNavShown ? styles.navigation___responsive : styles.navigation
+      }`}
+    >
+      <button
+        type="button"
+        className={styles.navigation__btn___hide}
+        onClick={() => setIsNavShown(false)}
+      >
+        <IoClose />
+      </button>
+      <ul
+        onClick={() => setIsNavShown(false)}
+        className={styles.navigation__linkList}
+      >
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive
+              ? styles.navigation__linkList__link___active
+              : styles.navigation__linkList__link
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/gallery"
+          className={({ isActive }) =>
+            isActive
+              ? styles.navigation__linkList__link___active
+              : styles.navigation__linkList__link
+          }
+        >
+          Gallery
+        </NavLink>
+        <NavLink
+          to="/music"
+          className={({ isActive }) =>
+            isActive
+              ? styles.navigation__linkList__link___active
+              : styles.navigation__linkList__link
+          }
+        >
+          Music
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive
+              ? styles.navigation__linkList__link___active
+              : styles.navigation__linkList__link
+          }
+        >
+          Contact
+        </NavLink>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,5 +1,19 @@
+import Navigation from "../../features/Navigation/Navigation";
+import { useState } from "react";
+
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  const [isNavShown, setIsNavShown] = useState(false);
+
+  const handleNavShown = () => {
+    setIsNavShown(!isNavShown);
+  };
+
+  return (
+    <>
+      <h1 onClick={handleNavShown}>tu będzie logo</h1>
+      <Navigation isNavShown={isNavShown} setIsNavShown={setIsNavShown} />
+    </>
+  );
 };
 
 export default Home;

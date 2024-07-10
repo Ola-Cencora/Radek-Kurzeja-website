bda0a18a295f45a6f589293ffb5f592559169665
Ola <cencora.aleksandra@gmail.com>
2024-07-10 11:36:50 +0200

    Add header with styles for mobile

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -1,5 +1,36 @@
+import Button from "../../common/Button/Button";
+import Logo from "../../common/Logo/Logo";
+import Navigation from "../../features/Navigation/Navigation";
+import { IoMenuSharp } from "react-icons/io5";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
 const Header: React.FC = () => {
-  return <p>Header</p>;
+  const [isNavShown, setIsNavShown] = useState(false);
+
+  const handleNavShown = () => {
+    setIsNavShown(!isNavShown);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.header__logo}>
+        <Logo />
+        <h1 className={styles.header__logo___name}>
+          Radosław <br />
+          Kurzeja
+        </h1>
+      </Link>{" "}
+      <Button
+        content={<IoMenuSharp />}
+        onClick={handleNavShown}
+        variant="navigation_btn"
+        ariaLabel="show navigation"
+      />
+      <Navigation isNavShown={isNavShown} setIsNavShown={setIsNavShown} />
+    </header>
+  );
 };
 
 export default Header;

60efe13d2e055eda660af47fb4742aa82d7ac2e5
Ola <cencora.aleksandra@gmail.com>
2024-08-01 15:33:49 +0200

    Add styles for music page and players

--- src/components/features/MusicPlayer/MusicPlayer.tsx
+++ src/components/features/MusicPlayer/MusicPlayer.tsx
@@ -1,18 +1,24 @@
+import styles from "./MusicPlayer.module.scss";
+
 interface MusicPlayerProps {
   title: string;
   src: string;
   href: string;
+  albumTitle: string;
 }
-const MusicPlayer: React.FC<MusicPlayerProps> = ({ title, src, href }) => {
+const MusicPlayer: React.FC<MusicPlayerProps> = ({
+  title,
+  src,
+  href,
+  albumTitle,
+}) => {
   return (
-    <iframe
-      title={title}
-      style={{ border: 0, width: "350px", height: "470px" }}
-      src={src}
-      seamless
-    >
-      <a href={href}>{title}</a>
-    </iframe>
+    <div className={styles.player}>
+      <h2 className={styles.player__title}>{albumTitle}</h2>
+      <iframe title={title} className={styles.player__box} src={src} seamless>
+        <a href={href}>{title}</a>
+      </iframe>
+    </div>
   );
 };
 

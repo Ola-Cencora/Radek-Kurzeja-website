578efb9f5e96ca707e304ead50462450d31db825
Ola <cencora.aleksandra@gmail.com>
2024-08-02 11:54:16 +0200

    Add photo to music subpage

--- src/components/pages/Music/Music.tsx
+++ src/components/pages/Music/Music.tsx
@@ -1,10 +1,20 @@
 import MusicPlayersList from "../../features/MusicPlayersList/MusicPlayersList";
+import styles from "./Music.module.scss";
 
 const Music: React.FC = () => {
   return (
     <section>
       <h1>Music</h1>
-      <MusicPlayersList />
+      <div className={styles.columns}>
+        <div className={styles.columns__photo}>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/music.jpg`}
+            alt="Radosław Kurzeja playing a concert"
+            className={styles.columns__photo___img}
+          />
+        </div>
+        <MusicPlayersList />
+      </div>
     </section>
   );
 };

61d80ff0fdb3dbb5227c2fef37aca6bb0f9556ff
Ola <cencora.aleksandra@gmail.com>
2024-08-05 15:46:56 +0200

    Add access to data on single artwork page

--- src/components/pages/SingleArt/SingleArt.tsx
+++ src/components/pages/SingleArt/SingleArt.tsx
@@ -1,5 +1,25 @@
+import { useParams } from "react-router";
+import galleryData from "../Gallery/galleryData";
+
 const SingleArt: React.FC = () => {
-  return <div>single art page with gallery and full info</div>;
+  const { id } = useParams<{ id: string }>();
+  const art = galleryData
+    .flatMap((set) => set.works)
+    .find((work) => work.id === id);
+
+  if (!art) {
+    return <div>No such artwork</div>;
+  }
+
+  return (
+    <div>
+      <h1>{art.title}</h1>
+      <p>technique: {art.technique}</p>
+      <p>dimensions: {art.dimensions}</p>
+      <p>date: {art.date}</p>
+      <img src={art.src} alt={art.title} />
+    </div>
+  );
 };
 
 export default SingleArt;

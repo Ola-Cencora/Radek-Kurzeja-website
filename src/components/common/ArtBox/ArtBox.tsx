24a5f29c73fbc42e2ff7db1670100f87225c253c
Ola <cencora.aleksandra@gmail.com>
2024-08-05 16:25:46 +0200

    Add styles for ArtBox

--- src/components/common/ArtBox/ArtBox.tsx
+++ src/components/common/ArtBox/ArtBox.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { WorkData } from "../../pages/Gallery/galleryData";
+import styles from "./ArtBox.module.scss";
 
 const ArtBox: React.FC<WorkData> = ({ id, title, src }) => {
   return (
-    <div>
-      <h3>{title}</h3>
-      <Link to={id}>
-        <img src={src} alt={title} />
-      </Link>
-    </div>
+    <Link className={styles.box} to={id}>
+      <img className={styles.box__img} src={src} alt={title} />
+      <div className={styles.box__img__overlay}>
+        <h3 className={styles.box__img__title}>{title}</h3>
+      </div>
+    </Link>
   );
 };
 

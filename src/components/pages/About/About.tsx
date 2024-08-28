8e53e5c339819f6bc3c861ec541c64a66dc302bb
Ola <cencora.aleksandra@gmail.com>
2024-08-28 12:31:24 +0200

    Add mock about text to about subpage

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,13 +1,36 @@
 import PageTitle from "../../common/PageTitle/PageTitle";
+import ScrollTopButton from "../../features/ScrollTopButton/ScrollTopButton";
 import useScroll from "../../../hooks/useScroll";
 import useTitle from "../../../hooks/useTitle";
+import { ABOUT_TXT } from "./aboutTxt";
+import { useState, useEffect } from "react";
+import styles from "./About.module.scss";
 
 const About: React.FC = () => {
+  const [paragraphs, setParagraphs] = useState<string[]>([]);
+
+  useEffect(() => {
+    setParagraphs(ABOUT_TXT.split("\n"));
+  }, []);
+
   useScroll();
 
   useTitle("About");
 
-  return <PageTitle title="About" />;
+  return (
+    <section className={styles.about}>
+      <PageTitle title="About" />
+      <div className={styles.about__txt}>
+        {paragraphs.map((paragraph, index) => (
+          <>
+            <p key={index}>{paragraph}</p>
+            <br />
+          </>
+        ))}
+      </div>
+      <ScrollTopButton />
+    </section>
+  );
 };
 
 export default About;
